dddfd5299ba3910946a583a00b0faf91f67a7f69
JCode <231753395+p2tch@users.noreply.github.com>
2026-03-14 20:39:51 +0100

    multimodule

---
+++ common/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "xyz.thehiddenobject"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(25)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,4 @@
 rootProject.name = "THOChat"
+
+include("paper")
+include("common")
